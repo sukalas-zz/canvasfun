a69fd02485d1c54bfa9ad8a4eda6daefa7c63704
sukalas <litridis@gmail.com>
2017-08-26 19:44:39 +0200

    added v12

---
+++ v12/main.js
@@ -0,0 +1,55 @@
+var main = function(){
+	var h = new helpers();
+
+	var particleNum = 10000;
+	var bool = false;
+	var frames = 0;
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	var canvas = h.c('canvas');
+	var ctx = canvas.getContext('2d');
+		canvas.width = width;
+		canvas.height = height;
+
+	let p = new Array();
+	p.width = 2;
+	p.height = p.width;
+	document.body.appendChild(canvas);
+	for(let i=0;i<=particleNum;i++){
+		p[i] = new Particle(i, Math.floor(width/2), Math.floor(height/2), p.width, p.height, width, height);
+	}
+
+	var loop = function(){
+		clean();
+		draw();
+		swim();
+		requestAnimationFrame(loop);
+		// console.log("loop", counter++)
+	}
+
+	var swim = function(){
+		for(let i=0;i<=particleNum;i++){
+			p[i].swim();
+		}
+	}
+
+	var draw = function(){
+		// ctx.restore(); // restore to the default state
+		for(let i=0;i<=particleNum;i++){
+			// ctx.restore(); // save the default state
+			// color = rgba(p[i].color.r, p[i].color.g, p[i].color.b, p[i].color.a);
+			// color = "rgba("+p[i].color.r+","+p[i].color.g+","+p[i].color.b+","+p[i].color.a+")";
+			ctx.fillStyle = p[i].colorFinal;
+			ctx.fillRect(p[i].posX, p[i].posY, p.width, p.height);
+			// ctx.save(); // save the default state
+		}
+
+	}
+
+	var clean = function(){
+		ctx.fillStyle = "rgba(0,0,0,.6)";
+		ctx.fillRect(0, 0, width, height);
+	}
+
+	loop();
+}
---
+++ v12/particle.js
@@ -0,0 +1,47 @@
+class Particle{
+  constructor(index, posX, posY, width, height, innerWidth, innerHeight) {
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+  	this.innerWidth = innerWidth;
+  	this.innerHeight = innerHeight;
+    this.speed = .3;
+    this.directionX = 1;
+    this.directionY = 1;
+	this.color = {r:255,g:255,b:255, a:Math.random()*(0.8 + 0.3) - 0.3};
+    this.visited = new Array();
+    this.particle = new Array();
+    this.rad = 2*Math.PI/180;
+    this.life = 100;
+    this.index = index;
+	this.particle[this.index] = this;
+  }
+  
+ swim(){
+	this.rad += .135;
+
+	this.posX += this.speed * this.directionX;
+	this.posY += this.speed * this.directionY;
+
+	this.speed += Math.sin(this.rad)/10;
+
+	if(Math.random()<.5){
+		this.directionX = Math.random()>.5?this.directionX=1:this.directionX=-1;
+		this.directionY = Math.random()>.5?this.directionY=1:this.directionY=-1;
+	}
+
+	this.color.r = Math.random()*255;
+	this.color.g = Math.random()*255;
+	this.color.b = Math.random()*255;
+	this.color.a = Math.random();
+
+
+	this.colorFinal = "rgba("+this.color.r+","+this.color.g+","+this.color.b+","+this.color.a+")";
+
+	if(Math.random()<0.05)this.rad = 0;
+
+	return this.colorFinal; 
+  }
+
+}
